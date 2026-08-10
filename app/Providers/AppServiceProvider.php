9ca6bb3545018a67489c1d85d7dcff6664f0e353
GillyGraphix <gilbertamani29@gmail.com>
2026-08-10 09:57:40 +0300

    changes in bootstrap folder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Lazimisha matumizi ya HTTPS kwenye link zote za picha, CSS, na JS tukiwa live
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
